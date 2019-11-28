76e9bde08330257eee3d77bddbe34b1cb76af9ed
VirtualGoat <nagarkarparth875@gmail.com>
2019-11-29 03:20:21 +0530

    Adding description

--- Linear Regression/Simple_Linear_Regression.py
+++ Linear Regression/Simple_Linear_Regression.py
@@ -5,38 +5,41 @@ Created on Fri Nov 29 00:47:45 2019
 """
 
 '''
-Concept: 
+-----------------------------------------Concept:-----------------------------------
 
-The equation of simple linear regression is: y=mx+c
-Linear Regression assumes a linear relationship between a the variables 'x' and a single variable 'y'.
+--->The equation of simple linear regression is: y=mx+c
+--->Linear Regression assumes a linear relationship between a the variables 'x' and a single variable 'y'.
 
-The regression line is plotted and the equation of the line is y=B0+B1x,
-where B0 and B1 are the parameters of the model and x is the input parameter given to the model. 
-Then the value of y is calculated. 
-
-
-The catch lies in determining the values of the parameters of the model: c and m (B0 and B1).
-
-To determine the values, we use the following formula:
-B1= (x'.y'-(xy)')/((x'.x')-(x.x)')
-B0= y'-B1.x'
+--->The regression line is plotted and the equation of the line is y=B0+B1x,
+    where B0 and B1 are the parameters of the model and x is the input parameter given to the model. 
+    Then the value of y is calculated. 
 
 
-Where, x' and y' indicate the mean values of x and y respectively. 
+--->The catch lies in determining the values of the parameters of the model: c and m (B0 and B1).
 
-After determining the values of B0 and B1, we just need to subsitute the input value of X in the equation.
+--->To determine the values, we use the following formula:
+    B1= (x'.y'-(xy)')/((x'.x')-(x.x)')
+    B0= y'-B1.x'
 
 
-Any prediction
+--->Where, x' and y' indicate the mean values of x and y respectively. 
+
+--->After determining the values of B0 and B1, we just need to subsitute the input value of X in the equation.
+
+--->After making the predictions, we need to determine the error in our predictions. 
+    for that, we use the r-squared method. 
+    The formula for r-squared method is: 
+        r=1-(ssr/sst)
+    Where, ssr= Error in regression line (y-y_pred)
+           sst= Deviation in mean (y-y')
+
 '''
-
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import datasets
+from sklearn.metrics import r2_score
 import pandas as pd
 from statistics import mean
 df=pd.read_csv('Salary_Data.csv')
@@ -45,31 +48,38 @@ df.fillna(df.mean(),inplace=True)
 x=df['YearsExperience'].values
 y=df['Salary'].values
 
+# =============================================================================
+# Dividing the dataset into Training and testing set. Training set is used to train the model and 
+# testing set is used to test the model's prediction
+# =============================================================================
+
 X_train,X_test,y_train,y_test=train_test_split(x,y,test_size=0.3)
 
-# =============================================================================
-# x=np.array([1,2,3,4,5],dtype=np.float64)
-# y=np.array([5,4,5,6,1],dtype=np.float64)
-# 
-# =============================================================================
+#Here, the best fit slope i.e the parameters B0 and B1 (m and b) will get calculated.
 def best_fit_slope(x,y):
     m= ((mean(x)*mean(y))-mean(x*y))/((mean(x)*mean(x))-mean(x*x))
     b= (mean(y)-m*mean(x))
     return m,b
 
 m,b=best_fit_slope(X_train,y_train)
+
+#Just subsituting the value of latest x input in the parameters calculated earlier. 
 def predict(m,x,b):
     y=(m*x)+b
     return y
 
 pred_values=[predict(m,i,b) for i in X_test]
 
+
+#Visualizing the results.
 plt.scatter(X_test, y_test, color = 'red')
 plt.plot(X_test, pred_values, color = 'blue')
-plt.title('Salary vs Experience (Full set)')
+plt.title('Salary vs Experience (Test set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
+
+#Calculating the r-squared error.
 ssr=0
 sst=0
 for i in range(len(y_test)):
@@ -77,31 +87,26 @@ for i in range(len(y_test)):
     sst+=(y_test[i]-mean(y_test))**2
 r=1-(ssr/sst)
 
-print(r)
-from sklearn.metrics import r2_score
+print("R-Squared error is: ",r)
 
-print(r2_score(y_test,pred_values))
 
-#X_train,X_test,y_train,y_test=train_test_split(x,y,test_size=0.3)
+# =============================================================================
+# In the code below, I have used the sklearn library to predict the values. 
+# =============================================================================
+x_train,x_test,Y_train,Y_test=train_test_split(x,y,test_size=0.3)
 regressor=LinearRegression()
-X_train=X_train.reshape(-1,1)
-y_train=y_train.reshape(-1,1)
+x_train=x_train.reshape(-1,1)
+Y_train=Y_train.reshape(-1,1)
 
-regressor.fit(X_train,y_train)
-X_test=X_test.reshape(-1,1)
-y_pred=regressor.predict(X_test)
-print(regressor.predict(pred))
-plt.scatter(X_train,y_train,color='red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-plt.title('Salary vs Experience (Training set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
+regressor.fit(x_train,Y_train)
+x_test=x_test.reshape(-1,1)
+y_pred=regressor.predict(x_test)
 y_test=y_test.reshape(-1,1)
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_test, regressor.predict(X_test), color = 'blue')
+plt.scatter(x_test, Y_test, color = 'red')
+plt.plot(x_test, y_pred, color = 'blue')
 plt.title('Salary vs Experience (Test set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
 
+print("R-Squared error is: ",r2_score(y_test,pred_values))
