9557f7a9858d858caee6a10ec31a040e262fd68b
VirtualGoat <nagarkarparth875@gmail.com>
2019-11-30 01:14:15 +0530

    Multiple Regression

--- Linear Regression/Multiple_Linear_Regression.py
+++ Linear Regression/Multiple_Linear_Regression.py
@@ -22,27 +22,40 @@ Multiple Linear Regression.
     
 --->
 '''
+from sklearn.metrics import accuracy_score
 
+from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 
 df=pd.read_csv('headbrain.csv')
+x=df.iloc[:,:-1].values
+y=df.iloc[:,3].values
+
+X_train, X_test, y_train, y_test= train_test_split(x,y,test_size=0.2)
 
 
 
+cl=LinearRegression()
+cl.fit(X_train,y_train)
+
+y_pred=cl.predict(X_test)
+
+ssr=0
+sst=0
+
+for i in range(len(y_test)):
+    ssr+=(y_test[i]-y_pred[i])**2
+    sst+=(y_test[i]-mean(y_test))**2
+
+r2=1-(ssr/sst)
 
 
-
-
-
-
-
-
-
-
+print("R-Squared error is: ",r2_score(y_test,y_pred))
 
 
 
