fcb18bd66ab222a7f6114a1525891ebe568d8b39
VirtualGoat <nagarkarparth875@gmail.com>
2019-11-30 21:08:29 +0530

    Polynomial Regression

--- Regression/Polynomial Regression/Polynomial_Regression.py
+++ Regression/Polynomial Regression/Polynomial_Regression.py
@@ -10,4 +10,45 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+from sklearn.preprocessing import PolynomialFeatures
+
+df=pd.read_csv('Position_Salaries.csv')
+X=df.iloc[:,1].values
+y=df.iloc[:,2].values
+
+X=X.reshape(-1,1)
+
+'''
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
+'''
+
+
+X_poly=PolynomialFeatures(degree=3)
+PolyReg=X_poly.fit_transform(X)
+
+cl=LinearRegression()
+cl.fit(PolyReg,y)
+y_pred=cl.predict(PolyReg)
+
+
+plt.scatter(X, y, color = 'red')
+plt.plot(X, y_pred, color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+print("r2 score:",r2_score(y,y_pred))
+
+
+
+
+
+
+
 
