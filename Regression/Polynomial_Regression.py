acf31cd4059e38846891d2d6f6691b6aa7b83432
VirtualGoat <nagarkarparth875@gmail.com>
2019-11-30 20:47:37 +0530

    Polynomial Regression

---
+++ Regression/Polynomial_Regression.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 20:39:02 2019
+
+@author: Parth
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
