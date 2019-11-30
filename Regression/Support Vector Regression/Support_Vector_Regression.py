b667fcaaacb93f857b29c3529131aef681b58cba
VirtualGoat <nagarkarparth875@gmail.com>
2019-12-01 01:31:31 +0530

    Support Vector Regression

--- Regression/Support Vector Regression/Support_Vector_Regression.py
+++ Regression/Support Vector Regression/Support_Vector_Regression.py
@@ -20,12 +20,10 @@ X=X.reshape(-1,1)
 y=y.reshape(-1,1)
 
 # =============================================================================
-# I have commented the feature scaling code and haven't executed it because in Linear 
-# Regression models the feature scaling is done implicitly by the library. But in some uncommon 
-# Regression models like SVR, etc. we need to explicitly scale the features as
-# the features aren't implicitly scaled by the library. 
+# The StandardSclaer library transform the data such that the mean of the dataset 
+# becomes 0 and the standard deviation becomes 1. It does so by subtracting every sample
+# value by the dataset's mean and dividing the subtracted value by the standard deviation.
 # =============================================================================
-
 
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
@@ -37,9 +35,16 @@ from sklearn.svm import SVR
 regressor= SVR(kernel='rbf')
 regressor.fit(X,y)
 
+# =============================================================================
+# -->In order to make custom predictions, the data must be in an array format. Hence 
+#    I have created an Numpy array and passed the value as input. 
+# -->As the regressor was trained on scaled data, we must also pass the scaled data as
+#    input to the regressor. 
+# -->The prediction that we will get will also be in the scaled form. So we need to convert 
+#    it back from scaled form to the normal form. This is done using inverse_transform method. 
+# =============================================================================
+
 y_pred=sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
-
-
 
 plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
